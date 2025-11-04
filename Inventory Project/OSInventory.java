59ec3aadc5a81ca6267ffa69de5b51225b0ab7b7
Ryan Powell <hbmftw@yahoo.com>
2025-11-04 14:22:34 -0700

    Refactor OSInventory into VisualGUI class

--- Inventory Project/OSInventory.java
+++ Inventory Project/OSInventory.java
@@ -1,23 +1,7 @@
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JOptionPane;
-import javax.swing.BoxLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.Color;
 
-
-
-public class OSInventory {
-    
+public class VisualGUI {
     private static JFrame mainInventoryFrame;
     private static JMenuBar menuBar;
     private static JMenu fileMenu;
@@ -28,10 +12,9 @@ public class OSInventory {
 
     public OSInventory() {
         // Constructor
-    }   
+    } 
 
-   
-
+    //MAIN
     public static void main(String[] args) {
         // Application entry point
         OSInventory main = new OSInventory();
@@ -40,92 +23,219 @@ public class OSInventory {
         main.fileMenu();
         main.editMenu();
         main.helpMenu();
-    }
-
-//---------------------------------------------------------------------------------------------------------
-
-
-    //File Menu
-    public void fileMenu() {
-        fileMenu = new JMenu("File");
-        JMenuItem exitItem = new JMenuItem("Exit");
-        exitItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-        fileMenu.add(exitItem);
-        menuBar.add(fileMenu);
-    }
-
-    //Edit Menu
-    public void editMenu() {
-        editMenu = new JMenu("Edit");
-        // Add edit menu items here
-        menuBar.add(editMenu);
-    }
-
-    //Help Menu
-    public void helpMenu() {
-        helpMenu = new JMenu("Help");
-        JMenuItem aboutItem = new JMenuItem("About");
-        aboutItem.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(mainInventoryFrame, "OS Inventory Application\nVersion 1.0");
-            }
-        });     
-        helpMenu.add(aboutItem);
-        menuBar.add(helpMenu);
-    }  
-
-    //Main Frame
-    public void mainFrame() {
-        mainInventoryFrame = new JFrame("OS Inventory");
-        mainInventoryFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainInventoryFrame.setSize(800, 600);
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        inventoryTable = new JTable(); // Placeholder for inventory table
-        JScrollPane scrollPane = new JScrollPane(inventoryTable);
-        mainPanel.add(scrollPane);
-        mainInventoryFrame.add(mainPanel);
-        mainInventoryFrame.setVisible(true);
 
         //JFrame to exit on close
-        mainInventoryFrame.addWindowListener(new java.awt.event.WindowAdapter() {
+        main.addWindowListner(new WindowAdapter() {
             @Override
-            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+            public void windowClosing(WindowEvent windowEvent) {
                 System.exit(0);
             }
         });
+
+        //Make JFrame visible
+        main.setVisible(true);
     }
 
-    //Menu Bar
-    public void showMenu() {
-
-        //Create Menu Bar
-        final JMenuBar menuBar = new JMenuBar();
+    // MENU BAR Constructor
+    void showMainMenu(){
+        final JMenuBar mainMenuBar = new JMenuBar();
 
         //Add Menu Tabs to Menu Bar
-        menuBar.add(fileMenu);
-        menuBar.add(editMenu);
-        menuBar.add(helpMenu);
+        mainMenuBar.add(fileMenu());
+        mainMenuBar.add(editMenu());
+        mainMenuBar.add(helpMenu());
 
-        //Set Menu Bar to Frame
-        mainInventoryFrame.setJMenuBar(menuBar);
-
-        //Set Menu Bat Background Color
-        menuBar.setBackground(Color.GRAY);
-
-        //Remove Border around Menu Bar
-        menuBar.setBorderPainted(false);
-
-        //Set Menu Bar Opaque to true
-        menuBar.setOpaque(true);
+        //MENU bar settings
+        mainMenuBar.setBackground(Color.GRAY);
+        mainMenuBar.setBorderPainted(false);
+        mainMenuBar.setOpaque(true);
 
         
-        mainInventoryFrame.setVisible(true);
-    }   
-//---------------------------------------------------------------------------------------------------------
+        //Set MENU bar to JFrame
+        main.setJMenuBar(mainMenuBar);
+    
+    }
+
+
+    //FILE menu items function
+	private JMenu fileMenu() {
+		//------------FILE MENU ITEMS----------------
+
+		JMenu fileMenu = new JMenu("FILE");
+
+		//Open Database menu item
+		JMenuItem openDataBase = new JMenuItem("Open Database");
+		openDataBase.setActionCommand("openDB");
+
+		//Backup Database menu item
+		JMenuItem backupDataBase = new JMenuItem("Backup Database");
+		backupDataBase.setActionCommand("backupDB");
+
+		//Print Menu item to print current frame
+		JMenuItem printItem = new JMenuItem("Print");
+		printItem.setActionCommand("print");
+
+		//Exit menu item with action command "exit"
+		JMenuItem exitInventoryProgram = new JMenuItem("Exit");
+		exitInventoryProgram.setActionCommand("exit");
+
+		//Add action listeners to file menu items
+		openDataBase.addActionListener(new openDataBaseListener());
+		backupDataBase.addActionListener(new backupDataBaseListener());
+		printItem.addActionListener(new printItemListener());
+		exitInventoryProgram.addActionListener(new exitInventoryProgramListener());
+
+
+		// Add menu items to the FILE menu
+		fileMenu.add(openDataBase);
+		fileMenu.add(backupDataBase);
+		fileMenu.add(printItem);
+		fileMenu.add(exitInventoryProgram);
+
+		return fileMenu;
+		
+	}
+
+    //EDIT menu items function
+	private JMenu editMenu() {
+		//------------EDIT MENU ITEMS----------------
+
+		JMenu editMenu = new JMenu("EDIT");
+
+		//EDIT items
+		JMenuItem addItem = new JMenuItem("Add Item");
+		addItem.setActionCommand("addItem");
+
+		//Delete items
+		JMenuItem deleteItem = new JMenuItem("Delete Item");
+		deleteItem.setActionCommand("deleteItem");
+
+		//Modify items
+		JMenuItem modifyItem = new JMenuItem("Modify Item");
+		modifyItem.setActionCommand("modifyItem");
+
+		//Add action listeners to EDIT menu items
+		addItem.addActionListener(new addItemListener());
+		deleteItem.addActionListener(new deleteItemListener());
+		modifyItem.addActionListener(new modifyItemListener());	
+		
+		// Add menu items to the EDIT menu
+		editMenu.add(addItem);
+		editMenu.add(deleteItem);
+		editMenu.add(modifyItem);
+	
+		return editMenu;
+	}
+
+    //HELP menu items function returns HELP menu object	
+	private JMenu helpMenu() {
+		
+		JMenu helpMenu = new JMenu("HELP");
+
+		//About Program menu item
+		JMenuItem aboutProgram = new JMenuItem("About Program");
+		aboutProgram.setActionCommand("aboutProgram");
+
+		//Contact Support menu item
+		JMenuItem contactSupport = new JMenuItem("Contact Support");
+		contactSupport.setActionCommand("contactSupport");
+
+		//Check for Updates menu item
+		JMenuItem checkForUpdates = new JMenuItem("Check for Updates");
+		checkForUpdates.setActionCommand("checkForUpdates");
+
+		//Add action listeners to HELP menu items
+		aboutProgram.addActionListener(new aboutProgramListener());
+		contactSupport.addActionListener(new contactSupportListener());
+		checkForUpdates.addActionListener(new checkForUpdatesListener());
+
+		// Add menu items to the HELP menu
+		helpMenu.add(aboutProgram);
+		helpMenu.add(contactSupport);
+		helpMenu.add(checkForUpdates);
+
+		return helpMenu;
+	}
+
+    //----------------------Listeners--------------------------------------
+    //Action Listeners for menu items
+
+	//File Menu Action Listeners------------------------
+	class openDataBaseListener implements ActionListener{
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("Open Database Clicked");
+			//database_connect.main(null);
+			JPanel dataPanel = createDataPanel();
+			myFrame.getContentPane().removeAll();
+			myFrame.getContentPane().add(dataPanel);
+			myFrame.revalidate();
+			myFrame.repaint();
+			
+		}
+	}
+	class backupDataBaseListener implements ActionListener{
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("Backup Database Clicked");
+			
+		}
+	}
+	class printItemListener implements ActionListener{
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("Print Clicked");
+			InventoryPrint inventoryPrint = new InventoryPrint();
+			inventoryPrint.printInventory();
+		}
+	}
+	class exitInventoryProgramListener implements ActionListener{
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("Exit Clicked");
+			System.exit(0);
+		}
+	}
+
+
+	//Edit Menu Action Listeners------------------------
+	class addItemListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("Add Item Clicked");
+		}
+	}
+	class deleteItemListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("Delete Item Clicked");
+		}
+	}
+	class modifyItemListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("Modify Item Clicked");
+		}
+	}
+	//Help Menu Action Listeners------------------------
+	class aboutProgramListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("About Program Clicked");
+		}
+	}
+	class contactSupportListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("Contact Support Clicked");
+		}
+	}
+	class checkForUpdatesListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			System.out.println("Check for Updates Clicked");
+		}
+	}
+
+	//General Menu Item Listener for Exit Command--------------------
+	class MenuItemListener implements ActionListener {
+		public void actionPerformed(ActionEvent event) {
+			String menuName = event.getActionCommand();
+			if (menuName.equals("exit")) {
+                // exit program if "EXIT" menu item is clicked
+				System.exit(0); 
+			}
+		}
+	}
 
 }
